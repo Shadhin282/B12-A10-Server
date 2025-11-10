d3741f7b4727203b3944accf07ba12a99bb8999e
shadin khan <13307041@iubat.edu>
2025-11-11 04:10:56 +0600

    middleware add

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+app.use(express.json())
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
