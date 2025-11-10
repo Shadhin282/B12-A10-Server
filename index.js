007dfda7043bc195f33538547e21bb33387301ab
shadin khan <13307041@iubat.edu>
2025-11-11 05:27:10 +0600

    set api by ID

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,6 +34,13 @@ async function run() {
       const data = req.body;
       const result = await propertiesCollection.find().toArray();
       res.send(result)
+    })
+
+    app.get('/properties/:id', async (req, res) => {
+      const { id } = req.params;
+      const query = {_id: new ObjectId(id)};
+      const result = await propertiesCollection.findOne(query)
+      res.send(result);
     })
 
 
